fcf3cd09ff5254012d593bae00c1786a481ba570
Jarek <jkolodziej58@outlook.com>
2021-03-25 20:59:39 +0100

    lab3

---
+++ labyMO/lab3.cpp
@@ -0,0 +1,411 @@
+
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <iomanip>
+
+
+namespace lab3
+{
+
+using namespace std;
+
+#pragma region Picard
+
+    static double picard(double (*fun)(double),double (*phi)(double),double xStart,int maxN , double tolx, double tolf)
+    {
+        double reziduum = abs(fun(xStart));
+        double xNew = NULL;
+        double xOld = xStart;
+        double error = NULL;
+        int n = 1;
+
+
+        cout << setw(20) << "new X"
+            << setw(20) << "reziduum"
+            << setw(20) << "error estimator" << "\n";
+
+        while (1)
+        {
+            xNew = phi(xOld);
+            reziduum = abs(fun(xNew));
+            error = abs(xOld - xNew);
+
+            cout << setw(20) << setprecision(10) << xNew
+                << setw(20) << setprecision(10) << reziduum
+                << setw(20) << setprecision(10) << error << endl;
+
+            if (n >= maxN)
+            {
+                cout << "iteration over extended";
+                break;
+            }
+            else if (error <= tolx)
+            {
+                cout << "result is in given error tolerance";
+                break;
+            }
+            else if (reziduum < tolf)
+            {
+                cout << "result is in given reziduum tolerance";
+                break;
+            }
+            else if (reziduum == 0)
+            {
+                cout << "perfect approximation has been achived!\n";
+                break;
+            }
+
+            xOld = xNew;
+            n++;
+        }
+
+        return xNew;
+    }
+    static void picardTest()
+    {
+        cout << "\naproximation for picards method for sin(x/4) - x = 0 is: " << picard(
+            [](double x) -> double {return sin(x / 4) - x; },
+            [](double x) -> double {return sin(x / 4); },
+            5.0,
+            1000,
+            0.000001,
+            0.000001) << endl << endl;
+
+        cout << "\naproximation for picards method for tan(2*x) - 1 - x = 0 is: " << picard(
+            [](double x) -> double {return tan(2 * x) - 1 - x; },
+            [](double x) -> double {return tan(2 * x) - 1; },
+            1.5,
+            100,
+            0.0001,
+            0.0001) << endl<<endl<<endl;
+    }
+#pragma endregion
+
+#pragma region bisection
+
+    static double bisection(double (*fun)(double),double jumpAmountOfAB,int numberOfJumps, int maxN, double tolx, double tolf)
+    {
+        double a = -2, b = 1;
+        double aNew = NULL, bNew = NULL;
+        double x = NULL;
+        int n = 0;
+        double reziduum = NULL;
+        double error = NULL;
+
+        while (signbit(fun(a)) == signbit(fun(b)) || n <= numberOfJumps)
+        {
+            n++;
+            a -= jumpAmountOfAB;
+            b += jumpAmountOfAB;
+        }
+        n = 1;
+
+        cout << "[a,b] = [" << a << ","<< b << "]\n";
+
+        cout << setw(20) << setprecision(10) << "new X"
+            << setw(20) << setprecision(10) << "reziduum"
+            << setw(20) << setprecision(10) << "error" << endl;
+
+
+        while (1)
+        {
+            x = (a + b) / 2.0;
+
+            if (signbit(x) == signbit(a))
+                a = x;
+            else
+                b = x;
+
+            error = abs((b - a) / 2);
+
+            reziduum = abs(fun(x));
+
+
+            cout << setw(20) << setprecision(10) << x
+                << setw(20) << setprecision(10) << reziduum
+                << setw(20) << setprecision(10) << error << endl;
+
+
+
+            if (reziduum < tolf)
+            {
+                cout << "result is in given reziduum tolerance";
+                break;
+            }
+            else if (error <= tolx)
+            {
+                cout << "result is in given error tolerance";
+                break;
+            }
+            else if (reziduum == 0)
+            {
+                cout << "perfect approximation has been achived!\n";
+                break;
+            }
+            else if (n >= maxN)
+            {
+                cout << "iteration over extended";
+                break;
+            }
+
+            n++;
+        }
+
+        return x;
+
+    }
+
+    static void bisectionTest()
+    {
+        cout << "\naproximation for bisection method for sin(x/4) - x = 0 is: " << bisection(
+            [](double y) -> double {return sin(y / 4) - y; },
+            0.01,
+            1000,
+            1000,
+            0.0001,
+            0.0001) << endl << endl;
+
+        cout << "\naproximation for bisection method for tan(2*x) - 1  - x = 0 is: " << bisection(
+            [](double x) -> double {return tan(2 * x) - 1 - x; },
+            0.01,
+            1000,
+            1000,
+            0.0001,
+            0.0001) << endl << endl;
+    }
+
+#pragma endregion
+
+#pragma region Newton
+    static double newton(double (*fun)(double),double (*funDev)(double),double xStart, int maxN, double tolx, double tolf)
+    {
+        double xPrev = xStart;
+        double x = xStart;
+        double xPrevOld = NULL;
+        int n = 1.0;
+        double reziduum = NULL;
+        double error = NULL;
+
+
+        cout << setw(20) << setprecision(10) << "new X"
+            << setw(20) << setprecision(10) << "reziduum"
+            << setw(20) << setprecision(10) << "error" << endl;
+
+
+        while (1)
+        {
+            if (funDev(xPrev) == 0)
+            {
+                cout << "deveritive of function in new X is 0\n";
+                break;
+            }
+
+            x = xPrev - (fun(xPrev) / funDev(xPrev));
+            error = abs(x - xPrev);
+            reziduum = fun(x);
+
+
+            cout << setw(20) << setprecision(10) << x
+                << setw(20) << setprecision(10) << reziduum
+                << setw(20) << setprecision(10) << error << endl;
+
+
+
+            if (reziduum < tolf)
+            {
+                cout << "result is in given reziduum tolerance";
+                break;
+            }
+            else if (error <= tolx)
+            {
+                cout << "result is in given error tolerance";
+                break;
+            }
+            else if (reziduum == 0)
+            {
+                cout << "perfect approximation has been achived!\n";
+                break;
+            }
+            else if (n >= maxN)
+            {
+                cout << "iteration over extended";
+                break;
+            }
+            else if (xPrevOld == x)
+            {
+                cout << "iteration are in a loop";
+                break;
+            }
+
+            xPrevOld = xPrev;
+            xPrev = x;
+            n++;
+
+        }
+
+        return x;
+
+    }
+
+    static void newtonTest()
+    {
+        cout << "\naproximation for Newton method for sin(x/4)  - x = 0 is: " << newton(
+            [](double x) -> double {return sin(x / 4) - x; },
+            [](double x) -> double {return cos(x / 4) / 4 - 1.0; },
+            0.2516,
+            1000,
+            0.0001,
+            0.0001) << endl << endl;
+
+
+        cout << "\naproximation for Newton method for tan(2*x) - 1  - x = 0 is: " << newton(
+            [](double x) -> double {return tan(2.0 * x) - 1 - x; },
+            [](double x) -> double {return 2.0 * 1 / pow(cos(2.0 * x), 2) - 1.0; },
+            0.2516,
+            1000,
+            0.0001,
+            0.0001) << endl << endl;
+    }
+
+#pragma endregion
+
+#pragma region secant
+
+    static double secant(double (*fun)(double), double xStart1,double xStart2, int maxN, double tolx, double tolf)
+    {
+        double xPrevOld = xStart2;
+        double xPrev = xStart1;
+        double x = NULL;
+
+        int n = 1;
+        double reziduum = NULL;
+        double error = NULL;
+
+
+        cout << setw(20) << setprecision(10) << "new X"
+            << setw(20) << setprecision(10) << "reziduum"
+            << setw(20) << setprecision(10) << "error" << endl;
+
+
+        while (1)
+        {
+            if ((xPrev - xPrevOld) == 0 || (fun(xPrev) - fun(xPrevOld) == 0))
+            {
+                cout << "cant divide by zero\n";
+                break;
+            }
+
+            x = xPrev - fun(xPrev) / ((fun(xPrev) - fun(xPrevOld)) / (xPrev - xPrevOld));
+            reziduum = abs(fun(x));
+            error = abs(x - xPrev);
+
+            cout << setw(20) << setprecision(10) << x
+                << setw(20) << setprecision(10) << reziduum
+                << setw(20) << setprecision(10) << error << endl;
+
+
+            if (reziduum < tolf)
+            {
+                cout << "result is in given reziduum tolerance";
+                break;
+            }
+            else if (error <= tolx)
+            {
+                cout << "result is in given error tolerance";
+                break;
+            }
+            else if (reziduum == 0)
+            {
+                cout << "perfect approximation has been achived!\n";
+                break;
+            }
+            else if (n >= maxN)
+            {
+                cout << "iteration over extended";
+                break;
+            }
+
+
+            xPrevOld = xPrev;
+            xPrev = x;
+            n++;
+        }
+        return x;
+    }
+
+    static void secantTest()
+    {
+        cout << "\naproximation for secant method for sin(x/4)  - x = 0 is: " << secant(
+            [](double x) -> double {return sin(x / 4) - x; },
+            0.2516,
+            1.24,
+            1000,
+            0.0001,
+            0.0001) << endl << endl;
+
+
+        cout << "\naproximation for secant method for tan(2*x) - 1  - x = 0 is: " << secant(
+            [](double x) -> double {return tan(2.0 * x) - 1 - x; },
+            0.2516,
+            1.24,
+            1000,
+            0.0001,
+            0.0001) << endl << endl;
+    }
+
+#pragma endregion
+
+    static void test()
+    {
+        char choice = '0';
+
+        while (choice != 'e')
+        {
+            
+            cout << "menu:\n";
+            cout << "1. Picard method\n";
+            cout << "2. Picard method\n";
+            cout << "3. Picard method\n";
+            cout << "4. Picard method\n";
+            cout << "'e' to exit\n";
+            cout << "choose from 1 to 4:";
+            cin >> choice;
+
+            switch (choice)
+            {
+                case '1':
+                {
+                    cout << "Picard method:\n";
+                    picardTest();
+                    break;
+                }
+                case '2':
+                {
+                    cout << "Bisection Method:\n";
+                    bisectionTest();
+                    break;
+                }
+                case '3':
+                {
+                    cout << "Netwon Method test:\n";
+                    newtonTest();
+                    break;
+                }
+                case '4':
+                {
+                    cout << "Secant Method test:\n";
+                    secantTest();
+                    break;
+                }
+                case 'e':
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+
+}
+
--- labyMO/labyMO.cpp
+++ labyMO/labyMO.cpp
@@ -1,3 +1,5 @@
+#include "lab3.cpp"
+
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -6,11 +8,13 @@
 #include <string>
 #include <sstream>
 
+
+
 using namespace std;
 
-namespace lab1 
+namespace lab1
 {
-#pragma region zaj1zad1
+#pragma region lab1
 
     template <typename T>
     void zad1Temp()
@@ -51,7 +55,7 @@ namespace lab1
 namespace lab2
 {
 
-#pragma region zaj1zad2
+#pragma region lab2
 
     double exponential(double x,int n)
     {
@@ -87,77 +91,76 @@ namespace lab2
     double TaylorFunction(double x)
     {
 
-        //1-exp(x):
-        int n = 100;
-        double ex = 0.0;
-
-        double facN,powX;
-
-        for (int i = 1; i < n; i++)
+        if (fabs(x) < 0.5)
         {
-            facN = 1.0;
-            for (int j = 1; j <= i; j++)
-            {
-                facN *= j;
-            }
-            powX = pow(-x, i);
-            ex += powX / facN;
-        }
+            //1-exp(x):
+            int n = 100;
+            double ex = 0.0;
 
-        //cout << ex << endl;
-        return -ex / x;
+            double facN, powX;
+
+            for (int i = 1; i < n; i++)
+            {
+                facN = 1.0;
+                for (int j = 1; j <= i; j++)
+                {
+                    facN *= j;
+                }
+                powX = pow(-x, i);
+                ex += powX / facN;
+            }
+
+            return -ex / x;
+        }
+        
+        return (1 - exp(-x)) / x;
 
     }
 
+    void test()
+    {
+        stringstream sstream;
+        string xString;
+        string fString;
+
+        double x = 0.0;
+        double fx = 0.0;
+
+        ifstream xData("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\XdataRef.txt");
+        ifstream fData("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\fdataRef.txt");
+        //ifstream logData("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\logdataRef.txt");
+        ofstream dataNormalFun("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\normalFun.txt");
+        ofstream dataTaylorFun("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\TaylorFun.txt");
+
+
+        while (getline(xData, xString) && getline(fData, fString))
+        {
+            sstream << xString;
+            sstream >> x;
+            sstream.clear();
+
+            sstream << fString;
+            sstream >> fx;
+            sstream.clear();
+
+            dataNormalFun << setprecision(20) << scientific << log10(x) << " " << setprecision(20) << scientific << log10(fabs(lab2::NormalFunction(x) - fx)) << "\n";
+            dataTaylorFun << setprecision(20) << scientific << log10(x) << " " << setprecision(20) << scientific << log10(fabs(lab2::TaylorFunction(x) - fx)) << "\n";
+        }
+
+
+        xData.close();
+        fData.close();
+        dataNormalFun.close();
+        dataTaylorFun.close();
+    }
 
 #pragma endregion
 }
 
+
 int main()
 {
-
-#pragma region zaj1zad1
-    //lab1::test();
-#pragma endregion
-
-#pragma region zaj1zad2
-
-    stringstream sstream;
-    string xString;
-    string fString;
-
-    double x = 0.0;
-    double fx = 0.0;
-
-    ifstream xData("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\XdataRef.txt");
-    ifstream fData("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\fdataRef.txt");
-    //ifstream logData("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\logdataRef.txt");
-    ofstream dataNormalFun("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\normalFun.txt");
-    ofstream dataTaylorFun("E:\\studia\\semestr 4\\Metody obliczeniowe\\laby\\plots\\lab2\\TaylorFun.txt");
-
-
-    while (getline(xData, xString) && getline(fData,fString))
-    {
-        sstream << xString;
-        sstream >> x;
-        sstream.clear();
-
-        sstream << fString;
-        sstream >> fx;
-        sstream.clear();
-
-        dataNormalFun << setprecision(20) << scientific << log10(x) << " " << setprecision(20) << scientific << log10(fabs(lab2::NormalFunction(x) - fx)) << "\n";
-        dataTaylorFun << setprecision(20) << scientific << log10(x) << " " << setprecision(20) << scientific << log10(fabs(lab2::TaylorFunction(x) - fx)) << "\n";
-    }
-
-
-    xData.close();
-    dataNormalFun.close();
-    dataTaylorFun.close();
-
-
-#pragma endregion
-
-
+    
+    lab3::test();
 
 }
